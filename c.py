ef3fb743b7c74c759832ef7ebecd90cf506ff1b7
Arshad Ahamed <ars007.smv@gmail.com>
2024-03-06 15:44:12 +0500

    Added C.py

---
+++ c.py
@@ -0,0 +1,15 @@
+def multiply_numbers():
+    # Input the first number
+    num1 = float(input("Enter the first number: "))
+
+    # Input the second number
+    num2 = float(input("Enter the second number: "))
+
+    # Multiply the two numbers
+    result = num1 * num2
+
+    # Print the result
+    print("The result of multiplying", num1, "and", num2, "is:", result)
+
+# Call the function to multiply two numbers
+multiply_numbers()
