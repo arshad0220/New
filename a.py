24800da991bc7a93f4713453adcdeb0a568a5295
Arshad Ahamed <ars007.smv@gmail.com>
2024-03-06 15:28:11 +0500

    Initial Commit

---
+++ a.py
@@ -0,0 +1,8 @@
+# Function to add 2 to a number and print the result
+def add2_and_print(number):
+    result = number + 2
+    print("Result after adding 2:", result)
+
+# Example usage
+number = int(input("Enter a number: "))
+add2_and_print(number)
